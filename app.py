ebf6242381f226de28a080b98541aa58b670d0fc
chatbot-accn <88087640+chatbot-accn@users.noreply.github.com>
2021-07-29 14:30:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -21,12 +21,20 @@ app = Flask(__name__)
 @app.route('/')  # this is the home page route
 @app.route('/webhook', methods=['POST'])
 def webhook():
-    return {
-        "fulfillmentText": 'Slot booking Success!!',
-        "source": 'webhook'
+  req = request.get_json(silent=True, force=True)
+  sum = 0
+  query_result = req.get('queryResult')
+  num1 = int(query_result.get('parameters').get('number'))
+  num2 = int(query_result.get('parameters').get('number1'))
+  sum = str(num1 + num2)
+  print('here num1 = {0}'.format(num1))
+  print('here num2 = {0}'.format(num2))
+  return {
+        "fulfillmentText": 'The sum of the two numbers is: '+sum,
+        "source": "webhookdata"
     }
-  
-
+    
+   
 # run Flask app
 if __name__ == "__main__":
     app.run()
